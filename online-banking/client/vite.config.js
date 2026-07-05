a15347b0b16377e58365c7da3246e826a3135a2d
default <driffinder@gmail.com>
2026-07-05 06:27:31 +0000

    Add allowedHosts to Vite config for dev server

--- online-banking/client/vite.config.js
+++ online-banking/client/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    allowedHosts: ['3001--main--envt5t--driff.aisoftdev.in'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
